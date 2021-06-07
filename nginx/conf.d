0226f9f2a4cf393ff8653cbfd44beb38d5041811
Arsenii <belyakov.arseny2014@gmail.com>
2021-06-07 21:03:28 +0300

    Сервак поднятб

--- nginx/conf.d
+++ nginx/conf.d
@@ -3,10 +3,10 @@ server {
     server_tokens off;
     server_name 84.252.134.74;
     location /static/ {
-        root /var/html/;
+        root /code/;
     }
     location /media/ {
-        root /var/html/;
+        root /code/;
     }
     location / {
         proxy_pass http://web:8000;
